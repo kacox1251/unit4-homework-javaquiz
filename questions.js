746a114d95da6cc5b4e59bfa1c814d2dafa802c3
kacox1251 <56182547+kacox1251@users.noreply.github.com>
2019-11-15 22:39:24 -0700

    commit 3 pseudo

--- questions.js
+++ questions.js
@@ -1,2 +1,17 @@
 //put all questions here
-//
+//make at least 5 questions
+//out all questions in a single array
+
+
+// var questions = [
+//     {
+//       title:
+//       choices:
+//       answer:
+//     },
+//     {
+//       title:
+//       choices:
+//       answer:
+//     },
+// ]
--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//
+//have the first screen with a start button that changes the screen to the first question, and starts the timer, and starts score keeping
+
+//check back to picture carousel to get a hint of what to do when changing questions
+//after question is answered, use an on click to change to the next question
+//score is calculated by time remaining, the right choice turns green
+//each wrong answer deducts x amount of time, the choice turns red
+//if found and have time maybe add sounds for right and wrong answers
+
+//create a timer to count down until the end
+//make total time (total # of questions)*(amount of time per question)
+//after time runs out, or player complates questions, change to screen that says "All done!"
+//completing questions sets timer to zero
+//have it read the players high score
+//have an input for players initials
+
+//after submitting initials, add the scores and initials to local storage
+//changes to screen that displays all the high scores from local storage
+//include a button that takes you back to the start of the quiz
+//include a button that clears all info from local storage
+//the timer disappears and the "view high scores" button disappears
