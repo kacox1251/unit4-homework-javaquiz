944076dbef60dc06167308d201eb4cf6d2068fe8
kacox1251 <56182547+kacox1251@users.noreply.github.com>
2019-11-17 18:39:05 -0700

    commit 4

--- questions.js
+++ questions.js
@@ -3,15 +3,35 @@
 //out all questions in a single array
 
 
-// var questions = [
-//     {
-//       title:
-//       choices:
-//       answer:
-//     },
-//     {
-//       title:
-//       choices:
-//       answer:
-//     },
-// ]
+var questions = [
+    {
+      title: "How do you add Javascript to your HTML?",
+      choices: ["<javascript></javascript>", "<java></java>", "<script></script>", "<addscript></addscript>"],
+      answer: "<script></script>"
+    },
+    {
+      title: "Which one of these is a proper data type?",
+      choices: ["boolean", "character", "number", "both 1 and 3"],
+      answer: "both 1 and 3"
+    },
+    {
+      title: "What is the correct way to write an array?",
+      choices: ["(1, 2, 3)", "[1, 2, 3]", "1, 2, 3", "{1, 2, 3}"],
+      answer: "[1, 2, 3]"
+    },
+    {
+      title: "How do you start a for loop?",
+      choices: ["for (i=0) {}", "for i=0", "for [i=0; i<3; i++]", "for (i=0; i<3; i++) {}"],
+      answer: "for (i=0; i<3; i++) {}"
+    },
+    {
+      title: "How do you create a function?",
+      choices: ["function functionName()", "function.functionName()", "function=functionName()", "function:functionName ()"],
+      answer: "function functionName()"
+    },
+    {
+      title: "How do you call a function?",
+      choices: ["call.functionName()", "functionName()", "You don't have to", "call functionName()"],
+      answer: "functionName()"
+    },
+]
--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-//have the first screen with a start button that changes the screen to the first question, and starts the timer, and starts score keeping
+var questionIndex = 0;
+
+
+
+
+//start button changes the screen to the first question, starts the timer, and starts score keeping
+function startGame() {
+    //create a timer to count down until the end
+    var count = 91;
+    var timer = setInterval(function() {
+        count--;
+        document.querySelector('#timer').innerText=count;
+        
+        if(count === 0){
+            clearInterval(timer);
+        }
+    }, 1000);
+    
+    document.querySelectorAll(".question-container").innerHTML = `
+    <h2 class="question-title"></h2>
+    <button class="choice"></button>
+    <button class="choice"></button>
+    <button class="choice"></button>
+    <button class="choice"></button>`
+}
+
 
 //check back to picture carousel to get a hint of what to do when changing questions
 //after question is answered, use an on click to change to the next question
@@ -6,13 +31,10 @@
 //each wrong answer deducts x amount of time, the choice turns red
 //if found and have time maybe add sounds for right and wrong answers
 
-//create a timer to count down until the end
-//make total time (total # of questions)*(amount of time per question)
-//after time runs out, or player complates questions, change to screen that says "All done!"
+//after time runs out, or player completes questions, change to screen that says "All done!"
 //completing questions sets timer to zero
 //have it read the players high score
 //have an input for players initials
-
 //after submitting initials, add the scores and initials to local storage
 //changes to screen that displays all the high scores from local storage
 //include a button that takes you back to the start of the quiz
