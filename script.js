b6eece05f57c8b7a1efbef3f6c861b95546f6599
kacox1251 <56182547+kacox1251@users.noreply.github.com>
2019-11-17 19:44:43 -0700

    commit 5

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 var questionIndex = 0;
 
+var questionTitle = document.createElement("h2");
+questionTitle.setAttribute("class", "question-title");
+questionTitle.textContent = questions[questionIndex].title;
+
+document.querySelector(".question-container").append(questionTitle);
+
+var startBtn = document.querySelector("#start-button");
+
+startBtn.addEventListener("click", function(event) {
+    questionIndex++;
+    questionTitle.textContent = questions[questionIndex].title;
+    
+    for (var i = 0; i < questions[questionIndex].choices.length; i++) {
+        var choiceBtn = document.createElement("button");
+        choiceBtn.setAttribute("class", "choice-button");
+        choiceBtn.textContent = questions[questionIndex].choices[i];
+        document.querySelector(".question-container").append(choiceBtn);
+    }
+});
+
+//     var count = 91;
+//     var timer = setInterval(function() {
+//         count--;
+//         document.querySelector('#timer').innerText=count;
+        
+//         if(count === 0){
+//             clearInterval(timer);
+//         }
+//     }, 1000);
 
 
 
 //start button changes the screen to the first question, starts the timer, and starts score keeping
-function startGame() {
-    //create a timer to count down until the end
-    var count = 91;
-    var timer = setInterval(function() {
-        count--;
-        document.querySelector('#timer').innerText=count;
-        
-        if(count === 0){
-            clearInterval(timer);
-        }
-    }, 1000);
-    
-    document.querySelectorAll(".question-container").innerHTML = `
-    <h2 class="question-title"></h2>
-    <button class="choice"></button>
-    <button class="choice"></button>
-    <button class="choice"></button>
-    <button class="choice"></button>`
-}
-
 
 //check back to picture carousel to get a hint of what to do when changing questions
 //after question is answered, use an on click to change to the next question
