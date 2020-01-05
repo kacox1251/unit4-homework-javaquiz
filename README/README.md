ba566d682e7f3a4bec1f474cee23e8fc3e995d91
kacox1251 <56182547+kacox1251@users.noreply.github.com>
2020-01-05 01:58:56 -0700

    adding readme

--- README/README.md
+++ README/README.md
@@ -1,1 +1,51 @@
 # unit4-homework-javaquiz
+
+## Description
+
+This is the coding quiz for the Unit 4 homework.
+
+Future Features:
+    - Fun new theme.
+    - Multiple quiz types.
+    - New cover page to display all the different quizes, quizes will be selectable.
+    - Quizes more immerasable and responsive.
+
+## Installation
+
+In order to install this project follow the Github link to https://github.com/kacox1251/unit4-homework-javaquiz and fork the project. After that clone the repository through your Git Bash. To do that type 'git clone" and paste the HTML link inside your terminal, it will ask you for your github password. After the repository is safed in the directory of your choice you can access the HTML and CSS files and view them in your default browser.
+You can also download as a zip file by clicking "Download zip" in the "Clone or Download" dropdown.
+
+
+## Usage
+
+
+
+
+## Credits
+
+Credit to this project belongs to me.
+
+
+## Liscense
+
+MIT License
+
+Copyright (c) [2019] [Krystal Cox]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,37 +1,44 @@
+//variables
 var questionIndex = 0;
 var correct = 0;
 var incorrect = 0;
 var count = 90;
 var timer;
+var users = [];
+var scores = [];
 
-var questionTitle = document.querySelector(".question-title");
-var startBtn = document.querySelector("#start-button");
-var highScoresBtn = document.querySelector("#view-scores");
-var submitScoreBtn = document.querySelector("#submit-button");
-var optionsContainer = document.querySelector(".options-container");
-var highScoreEl = document.querySelector("#highscores");
 var frontPageEl = document.querySelector("#front-page");
+var highScoresBtn = document.querySelector("#view-scores");
+var startBtn = document.querySelector("#start-button");
+
+var timerEl = document.querySelector("#timer");
 var questionContainerEl = document.querySelector(".question-container");
-var yourScoreEl = document.querySelector(".your-score");
+var questionTitle = document.querySelector(".question-title");
+var optionsContainer = document.querySelector(".options-container");
+
+var allDoneEl = document.querySelector("#done");
 var correctEl = document.querySelector(".correct");
 var incorrectEl = document.querySelector(".incorrect");
-var allDoneEl = document.querySelector("#done");
-var timerEl = document.querySelector("#timer");
-var tryAgainBtn = document.querySelector("#try-again");
+var yourScoreEl = document.querySelector(".your-score");
+var userInitials = document.querySelector("#initials");
+var submitScoreBtn = document.querySelector("#submit-button");
 
+var highScoreEl = document.querySelector("#highscores");
+var highScoresList = document.querySelector("#score-list");
+var tryAgainBtn = document.querySelector("#try-again");
+var clearScoresList = document.querySelector("#clear");
 
 var hasHiddenClass = highScoreEl.classList.contains("hidden");
 
+//functions
 function increaseIndex() {
   return questionIndex++;
 }
 
 function checkAnswer(userChoice) {
   if (userChoice === questions[questionIndex].answer) {
-    console.log("correct");
     correct++;
   } else {
-    console.log("incorrect");
     count -= 15;
     incorrect++;
   }
@@ -61,38 +68,41 @@ function addButtonChoiceListener(event) {
     return;
   }
   nextQuestion();
-  console.log(event);
 }
 
 function endGame() {
   clearInterval(timer);
 
   questionContainerEl.classList.add("hidden");
+
   correctEl.textContent = correct;
   incorrectEl.textContent = incorrect;
   yourScoreEl.textContent = correct * 15;
+
   allDoneEl.classList.remove("hidden");
   timerEl.classList.add("hidden");
 }
 
+//events
 startBtn.addEventListener("click", function(event) {
-  timer = setInterval(function() {
-    document.querySelector("#timer").innerText = count;
-    count--;
+  document.querySelector("#timer").innerText = count;
 
+  timer = setInterval(function() {
+    count--;
+    document.querySelector("#timer").innerText = count;
+    
     if (count <= 0) {
       clearInterval(timer);
       endGame();
     }
-    console.log(count);
   }, 1000);
-
+  
   frontPageEl.classList.add("hidden");
-  highScoresBtn.classList.add("hidden");
   nextQuestion();
 
-  if (questionContainerEl.classList.contains("hidden")) {
+  if (questionContainerEl.classList.contains("hidden") || timerEl.classList.contains("hidden")) {
     questionContainerEl.classList.remove("hidden");
+    timerEl.classList.remove("hidden");
   }
 });
 
@@ -106,8 +116,26 @@ highScoresBtn.addEventListener("click", function(event) {
   }
 });
 
-
 submitScoreBtn.addEventListener("click", function(event) {
+  if (userInitials.value) {
+    users.push(userInitials.value);
+    localStorage.setItem("users:", users);
+    localStorage.getItem("users:");
+
+    scores.push(yourScoreEl.textContent);
+    scores.sort(function(a, b){return b-a});
+
+    localStorage.setItem("highscores", [scores]);
+    localStorage.getItem("highscores:");
+
+    var storageString = JSON.stringify(localStorage);
+    var storageParse = JSON.parse(storageString);
+
+    console.log(storageParse);
+  } else {
+    console.log("nope")
+  }
+
   if (hasHiddenClass) {
     highScoreEl.classList.remove("hidden");
     allDoneEl.classList.add("hidden");
@@ -120,20 +148,30 @@ submitScoreBtn.addEventListener("click", function(event) {
 tryAgainBtn.addEventListener("click", function(event) {
   questionIndex = 0;
   count = 90;
+  correct = 0;
+  incorrect = 0;
 
-
+  if (hasHiddenClass) {
+    highScoreEl.classList.add("hidden");
+    frontPageEl.classList.remove("hidden");
+  } else {
+    highScoreEl.classList.remove("hidden");
+    frontPageEl.classList.add("hidden");
+  }
 });
 
+clearScoresList.addEventListener("click", function(event) {
+  users = [];
+  scores = [];
+  localStorage.clear();
+});
 //TODO: Next steps:
 /* 
 1. We need to create a click handler for the submit on add initials (only should fire if the input value length is greater than 0)
-2.Click Handler should gather together the score and initials. Read from local storage, determine if in fact the current score belongs in the
+2. Click Handler should gather together the score and initials. Read from local storage, determine if in fact the current score belongs in the
 high scores. Add it in the appropriate place and save to local storage
 3. View High scores when trigger, should read from local storage and check for high scores and if they exist should display that data.
-4. High scores button should disappear after start button is pressed
-5. Reset all scores when go back button is pressed
-6. Go back button should do similar functions to the high scores button
 7. High scores need to be organized highest to lowest
 8. High scores need to go from local storage onto an ordered list
-
+9. Clear High scores needs to clear (and possibly also the element)
 */
